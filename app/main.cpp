5bbbc5d5bf6a72f857d27d48487f5d232ddbc13f
Hossam <7osssam@protonmail.ch>
2024-07-22 21:27:15 +0300

    Add application icon and set window icon

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,7 @@
 #include <QApplication>
 #include <QFontDatabase>
 #include <QFont>
+#include <QIcon>
 #include "AppManager.h" // Include your AppManager header
 
 int main(int argc, char* argv[])
@@ -14,11 +15,15 @@ int main(int argc, char* argv[])
 	int			  fontIdItalic = fontDatabase.addApplicationFont(":/fonts/FiraSans-Italic.ttf");
 	int			  fontIdUltra = fontDatabase.addApplicationFont(":/fonts/FiraSans-Ultra.ttf");
 
-
 	// Set default font for the application
 	QString fontFamily = "Fira Sans"; // Ensure this matches the actual family name
 	QFont	font(fontFamily);
 	a.setFont(font);
+
+	QIcon icon = QIcon(":/icons/bank.png");
+
+	// Set the application icon
+	a.setWindowIcon(icon);
 
 	// Create an instance of the main application manager
 	AppManager& appManager = AppManager::getInstance();
